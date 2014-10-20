e159409edce5d0037fdb4625fd9167be7c066508
llkongs <k-johnny@qq.com>
2014-10-20 13:39:57 +0800

    This is the core function of h-index

---
+++ corehindex.R
@@ -0,0 +1,16 @@
+# This is the core function of the h-index function. It will computes the exact 
+#  Hirsch's defined h-index.
+
+Hindex <- function(data, totct = "tc") {
+        a <- vector()
+        b <- vector()
+        tlc <- data[, totct]
+        for (i in 1:length(tlc)) {
+                a[i] <- ifelse(length(which(tlc >= i)) == i, i, 0)
+                b[i] <- ifelse(length(which(tlc >= i)) <= i,
+                               i, length(tlc))
+        }
+        q <- sum(a)
+        h <- ifelse(q == 0, min(b)-1,q)
+        h
+}
